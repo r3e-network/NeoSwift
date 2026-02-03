64e5f1f595ef10dab0a6397b39019d54c1c6f0e5
Jimmy <jimmy@r3e.network>
2026-02-03 15:03:06 +0800

    fix(swift): add Data.bytes extension for [UInt8] conversion
    The Swift compiler was failing because Data type doesn't have a .bytes
    property by default. Added extension to convert Data to Bytes ([UInt8]).

--- Sources/NeoSwift/utils/Array.swift
+++ Sources/NeoSwift/utils/Array.swift
@@ -1,4 +1,6 @@
 
+import Foundation
+
 public extension Array {
     
     static func + (lhs: Array<Element>, rhs: Element) -> Array<Element> {
@@ -7,6 +9,15 @@ public extension Array {
     
     static func + (lhs: Element, rhs: Array<Element>) -> Array<Element> {
         return [lhs] + rhs
+    }
+    
+}
+
+public extension Data {
+    
+    /// Convert Data to Bytes ([UInt8])
+    var bytes: Bytes {
+        return [UInt8](self)
     }
     
 }
